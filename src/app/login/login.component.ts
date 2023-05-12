f859278a95ca8920b42905d6c3043c4a222ae4af
Antonella Battista <antobattista89@gmail.com>
2023-05-12 20:32:48 -0300

    mejoras login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
 import {HttpClient} from "@angular/common/http";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
@@ -9,12 +10,29 @@ import {HttpClient} from "@angular/common/http";
 })
 export class LoginComponent implements OnInit{
 
+  formSignin: FormGroup;
+  mensaje: string;
 
-  constructor(protected router:Router, protected httpClient: HttpClient) {
-    console.log('login constructor');
-
+  constructor(protected router:Router, protected httpClient: HttpClient, private formBuilder: FormBuilder) {
+    this.mensaje="";
   }
 
   ngOnInit(): void {
+    this.formSignin = this.formBuilder.group({
+      email: new FormControl('',  Validators.required),
+      password: new FormControl('',  Validators.required),
+    });
   }
+
+  ingresar(){
+    if (this.formSignin.get('email')?.value == "usuario@test.com")
+    {
+      this.router.navigate(['/home'])
+    }else {
+      this.mensaje = "Acceso Invalido";
+    }
+  }
+
+
+
 }
