b14979693c66afb2c3a4e805c18b2a44f39b2309
Axel Abalos <axelabalosss@gmail.com>
2023-06-05 15:18:55 -0300

    ya los datos del detalle producto no estan hardcodeados

--- src/app/detalle-producto/detalle-producto.component.ts
+++ src/app/detalle-producto/detalle-producto.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-detalle-producto',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./detalle-producto.component.css']
 })
 export class DetalleProductoComponent {
+  nombre: string;
+  precio: number;
+  imageUrl: string;
+  descripcion: string;
 
+  constructor(private route: ActivatedRoute) {
+    this.route.params.subscribe((params: any) => {
+      this.nombre = params.nombre;
+      this.precio = params.precio;
+      this.imageUrl = params.imageUrl;
+
+      this.descripcion = params.descripcion;
+    });
+  }
 }
