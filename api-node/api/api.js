62daaf40461243fd83adb1932abb2690f0c3f31f
Antonella Battista <antobattista89@gmail.com>
2023-06-24 21:22:48 -0300

    cognito + login

--- api-node/api/api.js
+++ api-node/api/api.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const fs = require('fs');
 const cors = require("cors");
-const os = require('os');
-const servidorMail = require('../helpers/mailer');
+const AmazonCognitoIdentity = require('amazon-cognito-identity-js');
+const CognitoUserPool = AmazonCognitoIdentity.CognitoUserPool;
+
+
+//Define Datos Cognito
+const poolData = {
+  UserPoolId : "sa-east-1_l3EaiYZAh",
+  ClientId : "2g16n0k6ki6s2ebvddhsch5es8"
+};
+const userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+
+
 
 const app = express();
-
-
 
 let isLogin = () => false;
 
@@ -16,12 +24,12 @@ let logger = (req, res, next) => {
 }
 
 
+
 //Define middlewares
 app.use(express.json());
 app.use(cors());
 app.use(logger);
 app.use(express.static('../datos'));
-
 
 
 //Definir Endpoints
@@ -44,15 +52,67 @@ app.get('/getImagenPorId', (req, res) => {
 });
 
 
-app.post('/registrar', (req, res) => {
-  servidorMail.enviarMailValidacionDeMail().then((mensaje) => { res.send(mensaje)});
-});
 
-app.get('/verificar', (req, res) => {
-  res.send("Mail Verificado");
+app.post('/registrar', (req, res) => {
+  console.log("JSON:" + JSON.stringify(req.body));
+
+  var attributeList = [];
+
+  attributeList.push(new AmazonCognitoIdentity.CognitoUserAttribute({Name:"email",Value:req.body.email}));
+
+  /*attributeList.push(new AmazonCognitoIdentity.CognitoUserAttribute({Name:"name",Value:"Prasad Jayashanka"}));
+  attributeList.push(new AmazonCognitoIdentity.CognitoUserAttribute({Name:"preferred_username",Value:"jay"}));
+  attributeList.push(new AmazonCognitoIdentity.CognitoUserAttribute({Name:"gender",Value:"male"}));
+  attributeList.push(new AmazonCognitoIdentity.CognitoUserAttribute({Name:"birthdate",Value:"1991-06-21"}));
+  attributeList.push(new AmazonCognitoIdentity.CognitoUserAttribute({Name:"address",Value:"CMB"}));
+  attributeList.push(new AmazonCognitoIdentity.CognitoUserAttribute({Name:"phone_number",Value:"+5412614324321"}));
+  */
+
+  userPool.signUp(req.body.username, req.body.password, attributeList, null, function(err, result){
+    if (err) {
+      console.log(err);
+      res.json(err);
+      return;
+    }
+    cognitoUser = result.user;
+    console.log('user name is ' + cognitoUser.getUsername());
+    res.json({
+      bienvenido: `${cognitoUser.getUsername()}`
+    })
+  });
+
 });
 
 
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!')
+});
+
+
+app.post('/login', (req, res) => {
+  var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails({
+    Username : req.body.username,
+    Password : req.body.password,
+  });
+
+  var userData = {
+    Username : req.body.username,
+    Pool : userPool
+  };
+
+  var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
+
+  cognitoUser.authenticateUser(authenticationDetails, {
+    onSuccess: function (result) {
+      console.log('access token + ' + result.getAccessToken().getJwtToken());
+      console.log('id token + ' + result.getIdToken().getJwtToken());
+      console.log('refresh token + ' + result.getRefreshToken().getToken());
+      res.json({'resp':'OK'})
+    },
+    onFailure: function(err) {
+      console.log(err);
+      res.json({'resp':'Credenciales Incorrectas'})
+    },
+
+  });
 });
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,6 +4,8 @@ import {HttpClient} from "@angular/common/http";
 import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {Observable, share} from "rxjs";
 import {Usuario} from "../modelos/usuario";
+import {ServicioUsuario} from "../servicios/servicioUsuario";
+import {Respuesta} from "../modelos/respuesta";
 
 @Component({
   selector: 'app-login',
@@ -15,7 +17,7 @@ export class LoginComponent implements OnInit {
   formSignin: FormGroup;
   mensaje: string;
 
-  constructor(protected router: Router, protected httpClient: HttpClient, private formBuilder: FormBuilder) {
+  constructor(protected router: Router, protected httpClient: HttpClient, private formBuilder: FormBuilder, private servicioUsuario: ServicioUsuario) {
     this.mensaje = "";
   }
 
@@ -28,16 +30,21 @@ export class LoginComponent implements OnInit {
 
   ingresar() {
 
-    let usuarios: Observable<Usuario[]> = this.httpClient.get<Usuario[]>('../assets/usuarios.json')
-      .pipe(share());
-    usuarios.subscribe(
+    const usuario =
+      {
+        'username': this.formSignin.get('email')?.value,
+        'password': this.formSignin.get('password')?.value
+      }
+
+    let respuesta: Observable<Respuesta> = this.servicioUsuario.login(usuario).pipe(share());
+    respuesta.subscribe(
       value => {
 
-        if (value.some(rec => rec.mail == this.formSignin.get('email')?.value && rec.password == this.formSignin.get('password')?.value)) {
+        if (value.resp == "OK") {
           this.router.navigate(['/tienda-arte'])
         } else {
 
-          this.mensaje = "Acceso Invalido";
+          this.mensaje = value.resp;
         }
 
       },
